6a7fdb68b2b3dcb9c3beaaab726c2bf8e469402f
MentalOfCrow <145018989+MentalOfCrow@users.noreply.github.com>
2025-02-19 15:15:22 +0100

    Update index.php
    Co-Authored-By: johan0104 <145018177+johan0104@users.noreply.github.com>
    Co-Authored-By: AbdallahYnov <145696378+AbdallahYnov@users.noreply.github.com>

--- backend/views/index.php
+++ backend/views/index.php
@@ -21,18 +21,28 @@ if (session_status() == PHP_SESSION_NONE) {
 
     <!-- CONTENU PRINCIPAL -->
     <main class="content">
-        <section class="home">
-            <h1>Bienvenue sur la plateforme de gestion du FabLab</h1>
-            <p>Optimisez la gestion des impressions 3D en automatisant les commandes et en suivant l’état des imprimantes.</p>
-            <p>Grâce à notre solution, vous pouvez :</p>
-            <ul class="features-list">
-                <li>Créer et modifier vos commandes d'impression facilement.</li>
-                <li>Suivre en temps réel l’état de vos impressions et la disponibilité des imprimantes.</li>
-                <li>Planifier automatiquement vos impressions en fonction des ressources disponibles.</li>
-                <li>Visualiser vos statistiques d'utilisation et générer des rapports exportables.</li>
-                <li>Recevoir des notifications pour être informé de l’état de vos commandes.</li>
-            </ul>
-            <p>Notre plateforme intuitive vous permet de gérer vos impressions de manière efficace et d’optimiser l’utilisation des imprimantes 3D du FabLab.</p>
+        <section class="home-section-1">
+            <div class="left-container">
+                <h1>Bienvenue sur la plateforme de gestion du FabLab</h1>
+                <p>Optimisez la gestion des impressions 3D en automatisant les commandes et en suivant l’état des imprimantes.</p>
+            </div>
+            <div class="right-container">
+                <img src="/public/assets/images/lab.jpg" alt="Le FabLab" class="fablab-image">
+            </div>
+        </section>
+
+        <section class="home-section-2">
+            <div class="features-container">
+                <p class="title">Grâce à notre solution, vous pouvez :</p>
+                <ul class="features-list">
+                    <li>Créer et modifier vos commandes d'impression facilement.</li>
+                    <li>Suivre en temps réel l’état de vos impressions et la disponibilité des imprimantes.</li>
+                    <li>Planifier automatiquement vos impressions en fonction des ressources disponibles.</li>
+                    <li>Visualiser vos statistiques d'utilisation et générer des rapports exportables.</li>
+                    <li>Recevoir des notifications pour être informé de l’état de vos commandes.</li>
+                </ul>
+                <p class="final-text">Notre plateforme intuitive vous permet de gérer vos impressions de manière efficace et d’optimiser l’utilisation des imprimantes 3D du FabLab.</p>
+            </div>
         </section>
     </main>
 </div>
